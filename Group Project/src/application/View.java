428da6294b8420c43e0d79620deb16ba20756955
Misti <Misti@10.204.18.206>
2017-04-05 18:06:15 -0600

    ButtonBar session

--- Group Project/src/application/View.java
+++ Group Project/src/application/View.java
@@ -1,22 +1,25 @@
 package application;
 
-
-
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.ButtonBar.ButtonData;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
@@ -93,30 +96,20 @@ public class View {
 		BorderPane bottom = new BorderPane();
 		
 		VBox southOfList = new VBox();
-		//southOfList.setCenterShape(true);
 		
 		Button shuffleB = new Button ("Shuffle");
 		Button deleteB = new Button ("Delete");
 		
-		
-		//shuffleB.isScaleShape();
-		//shuffleB.isResizable();
-		
 		southOfList.getChildren().addAll(shuffleB, deleteB);
-		//southOfList.setCenterShape(true);
-		//southOfList.isCenterShape();
-		//southOfList.setScaleShape(true);
-		//southOfList.isFillWidth();
 		southOfList.setBackground(new Background(new BackgroundFill(Paint.valueOf("GREY"), null, null)));
 		VBox.setMargin(shuffleB, new Insets(10, 90, 10, 90));
 		VBox.setMargin(deleteB, new Insets(10, 90, 10, 90));
 		
 		Label songInfo=new Label("Song Info");
-		songInfo.setMinSize(1600.0, 80.0);
+		songInfo.setMaxSize(1600.0, 80.0);
 		
 		
 		bottom.setLeft(songInfo);
-		//bottom.setCenter(songInfo);
 		bottom.setRight(southOfList);
 		
 		root.setBottom(bottom);
@@ -124,12 +117,42 @@ public class View {
 		
 		BorderPane player = new BorderPane();
 		player.setPadding(new Insets(10, 10, 10, 10));
+		player.setStyle("-fx-background-color: Gainsboro;-fx-border-color: black;");
+		
+		ButtonBar controlBar = new ButtonBar();
+		
+		Button rewindB = new Button("Rewind");
+		Button previousB = new Button("Previous Track");
+		Button playB = new Button("Play");
+		Button nextB = new Button("Next Track");
+		Button fastforwardB = new Button("Fast Forward");
+		Button slomoB = new Button("Slow Motion");
+		Button stopB = new Button("Stop");
+		//Button volumeB = new Button("Volume");
+		//Button muteB = new Button("Mute");
+		
+		BorderPane buttons = new BorderPane();
+		controlBar.getButtons().addAll(rewindB, previousB, playB, nextB, fastforwardB, slomoB, stopB);
+		controlBar.setPadding(new Insets(20, 20, 20, 20));
+		buttons.setTop(controlBar);
+		
+		//DoubleProperty s = new DoubleProperty();
+		
+		ImageView cover = new ImageView();
+		//Image aCover = new Image(null /*Main.class.getResourcAsStream("pic.png")*/);
+		
+		HBox albumnCover = new HBox();
+		albumnCover.getChildren().add(cover);
+				
+		//player.setTop(insert volume and mute here);
+		player.setCenter(albumnCover);
+		player.setBottom(buttons);
 		
 		//player.setCenter(albumnCover);
 		
 		//player.setSouth(buttonBar);
 		
-		root.setLeft(player);
+		root.setCenter(player);
 		
 		Scene scene = new Scene(root,400,400);
 		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
